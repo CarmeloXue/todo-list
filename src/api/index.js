5569eab6a327cec303a9e5d2792bcd707361da5f
carmelo <xuehaotian@meituan.com>
2020-01-31 01:12:45 +0800

    feat:url

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const requset = axios.create({
     //just convienince to debug locally and online
-  baseURL: "http://127.0.0.1:3389"
+  baseURL: "http://ec2-52-221-181-89.ap-southeast-1.compute.amazonaws.com:3389"
 });
 export const getListByPagination = (pageIndex, pageSize) => {
   return requset.get("/api/v1/persons");
